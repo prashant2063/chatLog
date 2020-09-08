fb6377458b71e98ec2206e54d1a6f4dd176de563
Prashant GUpta <prashant2063@gmail.com>
2020-09-08 21:16:09 +0530

    integrated messagesUtil to database

--- utils/messagesUtil.js
+++ utils/messagesUtil.js
@@ -1,20 +1,81 @@
-// const mongoClient = require("mongodb").MongoClient;
+const mongoClient = require("mongodb").MongoClient;
 
-var messagesList = [];
+// var messagesList = [];
+var dbUrl = "mongodb://localhost:27017";
+var dbName = "dummyDb";
 
 //add new message to database
 function postMessage(message){
-    messagesList.push(message);
+    mongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, dbHost) => {
+        if (err) {
+            console.error("Error connecting to the server");
+        }
+        else {
+            var db = dbHost.db(dbName);
+            db.collection("messages", (err, coll) => {
+                if (err) {
+                    console.error("Error connecting to the collection");
+                }
+                else {
+                    coll.insertOne(message);
+                }
+            })
+        }
+    })
 }
 
 //get all messages of the specified room
-function getAllMessagesByRoom(roomName){
-    return messagesList.filter(message => message.roomName == roomName);
+function getAllMessagesByRoom(roomName,callback){
+    mongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, dbHost) => {
+        if (err) {
+            console.error("Error connecting to the server");
+        }
+        else {
+            var db = dbHost.db(dbName);
+            db.collection("messages", (err, coll) => {
+                if (err) {
+                    console.error("Error connecting to the collection");
+                }
+                else {
+                    coll.find({roomName}).toArray((err,res)=>{
+                        if (err) {
+                            console.error("Error while fetching messages");
+                        }
+                        else{
+                            callback(res);
+                        }
+                    });
+                }
+            })
+        }
+    })
 }
 
 //get all the messages
-function getAllMessages(){
-    return messagesList;
+function getAllMessages(callback){
+    mongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, dbHost) => {
+        if (err) {
+            console.error("Error connecting to the server");
+        }
+        else {
+            var db = dbHost.db(dbName);
+            db.collection("messages", (err, coll) => {
+                if (err) {
+                    console.error("Error connecting to the collection");
+                }
+                else {
+                    coll.find({}).toArray((err,res)=>{
+                        if (err) {
+                            console.error("Error while fetching messages");
+                        }
+                        else{
+                            callback(res);
+                        }
+                    });
+                }
+            })
+        }
+    })
 }
 
 module.exports = {postMessage, getAllMessages, getAllMessagesByRoom};
